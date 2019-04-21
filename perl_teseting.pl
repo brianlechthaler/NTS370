024e786131cb7c5b62809c8859ce7195fa200c5e
Brian Lechthaler <brianlechthaler@protonmail.ch>
2019-04-20 18:51:07 -0700

    Added final and some backlog
    this is probably going to be the last commit.

---
+++ perl_teseting.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+function(){
+print("Hello, World!");
+}
---
+++ project4-2/project4-2.js
@@ -0,0 +1,22 @@
+//PROJECT 4.2: Simple JS example
+//Brian Lechthaler
+//4/17/2019
+//NTS370/G.Miles
+
+//NB: The easiest way to run this is the nodejs commandline tool as depicted in attached screenshots
+
+//Declare the two integers to add
+var numX = 13;
+var numY = 37;
+
+//Add the two integers, store into var sumXY
+sumXY = numX + numY;
+
+//Output sum to console, or multiply sum by 3 and print to console if X = Y
+if (numX === numY) {
+    three = sumXY * 3;
+	//console.log just prints to console
+    console.log(three);
+} else {
+    console.log(sumXY);
+}
